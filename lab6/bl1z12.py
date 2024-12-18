afc99fbf4ebe85d4b22e7b5fb6486ec028c7dcdd
Ralchenya <ralcenak@gmail.com>
2024-12-18 15:36:19 +0300

    Delete lab6 directory

--- lab6/bl1z12.py
+++
@@ -1,9 +0,0 @@
-import math
-x = float(input('Введите значение x:'))
-y = float(input('Введите значение y:'))
-if (x < 0 and y < 0) or (x > 0 and y > 0):
-    s = math.sqrt(x * y)
-    print('Среднее геометрическое:', s)
-else:
-    s = (x + y) / 2
-    print('Среднее арифметическое:', s)
--- lab6/bl2z3.py
+++
@@ -1,9 +0,0 @@
-A = int(input('Введите число A:'))
-B = int(input('Введите число B:'))
-if A >= B:
-    print('A должно быть меньше B')
-else:
-    sum = 0
-    for number in range(A, B + 1):
-        sum += number ** 2
-    print('Сумма', A, 'до', B, 'равна:', sum)
